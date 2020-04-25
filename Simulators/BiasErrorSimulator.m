6249107f85a0006973c8a21d359834f5a512b415
Luis Miguel Lopez <luismiguel.lopez@uia.no>
2020-04-25 16:03:31 +0200

    hungarian notation in BiasErrorSimulator

--- Simulators/BiasErrorSimulator.m
+++ Simulators/BiasErrorSimulator.m
@@ -7,7 +7,8 @@ classdef BiasErrorSimulator
     end
     
     methods
-        function [featDiff, locErrors] = ...
+        function [m_featDiff, v_locErrors, m_trueTDoAs, m_allLocBasedFeatures,...
+                m_estim_loc_tm_rx, m_trueLoc_all_points] = ...
                 simulate ( obj, m_grid_x, m_grid_y )
             %inputs:
             % m_grid_x: matrix containing the x-coordinates of all grid points
@@ -22,12 +23,12 @@ classdef BiasErrorSimulator
             %  for all points in the grid
             
             disp('Precomputing pilot signals...')
-            trueLoc_all_points  = zeros([2, num_all_points]);
+            m_trueLoc_all_points  = zeros([2, num_all_points]);
             t4_nps = zeros([n_sources, ... % noiseless pilot signals
                 obj.generator.maxSamplesPerPilot,size(m_grid_x)]);
             ltc = LoopTimeControl(numel(m_grid_x));
             for ind_gridPoint = 1:numel(m_grid_x)
-                trueLoc_all_points(:, ind_gridPoint) =...
+                m_trueLoc_all_points(:, ind_gridPoint) =...
                     [m_grid_x(ind_gridPoint), m_grid_y(ind_gridPoint)]';
                 [ind_row, ind_col] = ind2sub(size(m_grid_x), ind_gridPoint);
                 t4_nps(:,:,ind_row, ind_col) = ...
@@ -40,24 +41,24 @@ classdef BiasErrorSimulator
             t4_receivedPilotSignals = t4_nps + ... % taken from Sampler
                 obj.sampler.pilotNoiseSTD/sqrt(2) *( ...
                 randn(size(t4_nps)) + 1i*randn(size(t4_nps)) );
-            
+            % Reshape tensor to be used by featureExtractor:
             v_size = size(t4_receivedPilotSignals);
             t3_reshaped_receivedPS = reshape(t4_receivedPilotSignals, ...
                 [v_size(1:2), prod(v_size(3:4))]);
             
-            t_allLocBasedFeatures = obj.featureExtractor.locBasedExtract(...
+            m_allLocBasedFeatures = obj.featureExtractor.locBasedExtract(...
                 t3_reshaped_receivedPS); %Time differences of arrival
             
             % obtain true TDoAS
-            disp('Computing true TDoAs...')
-            trueTDoAs = obj.featureExtractor.obtainRangeDiffer(...
-                source_loc,trueLoc_all_points);   
-%             estim_loc_tm_rx = obj.locEstimator.estimateLocationRobustSDR(...
-%                 locBasedFeat_tracemap);
+            m_trueTDoAs = obj.featureExtractor.obtainRangeDiffer(...
+                source_loc,m_trueLoc_all_points);   
+            disp('Computing TDoA-based position estimates...')
+            m_estim_loc_tm_rx = obj.locEstimator.estimateLocations(...
+                m_allLocBasedFeatures);
            
             % compute localization errors and feature differences
-            featDiff = trueTDoAs - t_allLocBasedFeatures;
-            locErrors = 0; % vecnorm(trueLoc_tracemap - estim_loc_tm_rx);
+            m_featDiff = m_trueTDoAs - m_allLocBasedFeatures;
+            v_locErrors = vecnorm(m_trueLoc_all_points - m_estim_loc_tm_rx);
             disp('Done');
             
             
