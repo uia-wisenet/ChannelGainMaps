dcf7fedb2d79726f156abe3e2f8da4cdd209230d
Luis Miguel Lopez <luismiguel.lopez@uia.no>
2020-04-28 15:17:18 +0200

    updated Simulator2

--- Simulators/Simulator2.m
+++ Simulators/Simulator2.m
@@ -3,18 +3,25 @@ classdef Simulator2
         generator MapGenerator = PrecomputedMapGenerator();
         locFreeEstimator LocationFreeEstimator
         locBasedEstimator LocationBasedEstimator
-        hybridEstimator HybridEstimator
+        hybridEstimator HybridEstimator = HybridEstimator();
         locEstimator LocationEstimator_tdoa = DiverseLocationEstimator('');
         featureExtractor
         sampler % the only property we use from sampler is pilotNoiseSTD
         
         PCAenable = 0;
+        
+        b_trainLocFree = 1;
+        b_trainLocBased = 1;
+        b_trainHybrid = 0;
+        
+        b_cv = 0;
+        
+        b_syntheticLocError = 0
+        std_syntheticLocationNoise = 6;
     end
     
     methods
-        function [NMSE_locFree, NMSE_locBased, NMSE_hybrid, ...
-                locFreeMapEstimate,locBasedMapEstimate,...
-                hybridMapEstimate,channelForPairs_tracemap] = ...
+        function [str_NMSE, str_mapEstimates, channelForPairs_tracemap] = ...
                 simulate ( obj, m_train_pairs, m_tracemap_pairs, ...
                 m_grid_x, m_grid_y )
             %inputs: 
@@ -65,11 +72,11 @@ classdef Simulator2
                 t3_reshaped_receivedPS); %Time differences of arrival
             
             %% this section can be refactored by defining a static method
-            channelForPairsTr_noiseless=zeros(1,N_pair_train);
+            channelForPairsTr_noiseless=zeros(N_pair_train,1);
             n_sp_CoM  = size(t_allLocFreeFeatures,  1); % no. of source pairs
             n_sp_TDoA = size(t_allLocBasedFeatures, 1); % no. of source pairs
-            trueLoc_train  = zeros([2, size(m_train_pairs)]);
-            locFreeFeatures_train  = zeros([n_sp_CoM, size(m_train_pairs)]);
+            t3_trueLoc_train  = zeros([2, size(m_train_pairs)]);
+            locFreeFeatures_train  = zeros([n_sp_CoM,  size(m_train_pairs)]);
             locBasedFeatures_train = zeros([n_sp_TDoA, size(m_train_pairs)]);
             
             disp('Computing channel gains (for training)...')
@@ -86,8 +93,8 @@ classdef Simulator2
                     t_allLocFreeFeatures(:, [ind_tx ind_rx]);
                 locBasedFeatures_train(:, ind_pair, :) = ...
                     t_allLocBasedFeatures(:, [ind_tx ind_rx]);
-                trueLoc_train(:, ind_pair, 1) = xy_transmitter';
-                trueLoc_train(:, ind_pair, 2) = xy_receiver';
+                t3_trueLoc_train(:, ind_pair, 1) = xy_transmitter';
+                t3_trueLoc_train(:, ind_pair, 2) = xy_receiver';
                 ltc.go(ind_pair);
             end
             % add noise:
@@ -96,7 +103,7 @@ classdef Simulator2
                 randn(size(channelForPairsTr_noiseless));
             
             channelForPairs_tracemap = zeros(1, N_pair_tracemap); 
-            trueLoc_tracemap  = zeros([2, size(m_tracemap_pairs)]);
+            t3_trueLoc_tracemap   = zeros([2, size(m_tracemap_pairs)]);
             locFreeFeat_tracemap  = zeros([n_sp_CoM, size(m_tracemap_pairs)]);
             locBasedFeat_tracemap = zeros([n_sp_TDoA, size(m_tracemap_pairs)]);
             disp('Computing channel gains (noiseless, for map tracing)...')
@@ -113,76 +120,191 @@ classdef Simulator2
                     t_allLocFreeFeatures(:, [ind_tx ind_rx]);
                 locBasedFeat_tracemap(:, ind_pair, :) = ...
                     t_allLocBasedFeatures(:, [ind_tx ind_rx]);
-                trueLoc_tracemap(:, ind_pair, 1) = xy_transmitter';
-                trueLoc_tracemap(:, ind_pair, 2) = xy_receiver';
+                t3_trueLoc_tracemap(:, ind_pair, 1) = xy_transmitter';
+                t3_trueLoc_tracemap(:, ind_pair, 2) = xy_receiver';
                 ltc.go(ind_pair);
             end
+            
+            str_NMSE = struct();
+            str_mapEstimates = struct();
                      
             %%
             % here, the original simulator had some lines defining which
             % features to consider. This is skipped here.        
             
-            disp ('Training LocFree...')
-            my_locFreeTrainer = LocFreeTrainer;
-            my_locFreeTrainer.estimator = obj.locFreeEstimator;
-            %TODO: reshape?
-            [locFreeFKM, probMissingFeat] = my_locFreeTrainer.train(...
-                locFreeFeatures_train, channelForPairsTr);
-            disp('Evaluating LocFree at validation set...')
-            [meanErrOnEvalFeat,locFreeMapEstimate] = locFreeFKM.evaluate(...
-                locFreeFeat_tracemap);
-            disp('Done');
+            %%
             
-            disp ('Training LocBased...')
-            my_locBasedTrainer = LocBasedTrainer;
-            my_locBasedTrainer.estimator = obj.locBasedEstimator;
-            m_estim_loc_tx = obj.locEstimator.estimateLocations(... 
-                locBasedFeatures_train(:,:,1));
-            m_estim_loc_rx = obj.locEstimator.estimateLocations(...
-                locBasedFeatures_train(:,:,2));
-            % t3_estimatedLocation is a 3-way tensor: 
-            % (space_dim_index, sample_index, rx_or_tx)
-            t3_estim_loc_train = cat(3, m_estim_loc_tx, m_estim_loc_rx);
-            locBasedFKM = my_locBasedTrainer.train(...
-                t3_estim_loc_train,channelForPairsTr);
+            if obj.b_trainLocFree
+                disp ('Training LocFree...')
+                my_locFreeTrainer = LocFreeTrainer;
+                my_locFreeTrainer.estimator = obj.locFreeEstimator;
+                %%
+                v_lambdas_toTryLF = logspace(-4, -3, 14);    %TODO: move to properties
+                v_sigmas_toTryLF  = linspace(85, 200, 15);   %TODO: move to properties
+                if obj.b_cv
+                    disp ('Cross-validation...')
+                    [m_crossValScoresLF, best_lambdaLF, best_sigmaLF] = ...
+                        my_locFreeTrainer.crossValidate(...
+                        locFreeFeatures_train, channelForPairsTr, ...
+                        v_lambdas_toTryLF, v_sigmas_toTryLF);
+                    my_locFreeTrainer.estimator.regularizationParameter = best_lambdaLF;
+                    my_locFreeTrainer.estimator.kernel =  @(x, y) ...
+                        exp(-norms(x-y, 2, 1).^2/(best_sigmaLF^2));
+                    figure(998); clf
+                    mesh(v_sigmas_toTryLF, v_lambdas_toTryLF, m_crossValScoresLF); 
+                    ax = gca;
+                    ax.YAxis.Scale = 'log';
+                    xlabel \sigma
+                    ylabel \lambda
+                    title 'LOCATION FREE'
+                end
+                %%
+                [locFreeFKM, probMissingFeat] = my_locFreeTrainer.train(...
+                    locFreeFeatures_train, channelForPairsTr); %#ok<ASGLU>
+                %
+                disp('Evaluating LocFree at validation set...')
+                [str_mapEstimates.locFree, meanErrOnEvalFeat] = locFreeFKM.evaluate(...
+                    locFreeFeat_tracemap); %#ok<ASGLU>
+                disp('Done');
+                SqErr_locFree = Simulator.compute_SqErr(...
+                    channelForPairs_tracemap, str_mapEstimates.locFree);
+                str_NMSE.locFree = SqErr_locFree/sum((channelForPairs_tracemap...
+                    -mean(channelForPairs_tracemap)).^2);                
+            else
+                str_NMSE.locFree = 1;
+                str_mapEstimates.locFree = zeros(size(channelForPairs_tracemap));
+            end
             
-            disp('Evaluating LocBased at validation set...')
-            estim_loc_tm_tx = obj.locEstimator.estimateLocations(...
-                locBasedFeat_tracemap(:,:,1)); %estimated loc (tracemap)
-            estim_loc_tm_rx = obj.locEstimator.estimateLocations(...
-                locBasedFeat_tracemap(:,:,2));
-            % t3_estim_loc_tracemap is a 3-way tensor: 
-            % (space_dim_index, sample_index, rx_or_tx)
-            t3_estim_loc_tracemap = cat(3, estim_loc_tm_tx, estim_loc_tm_rx);
-            locBasedMapEstimate = locBasedFKM.evaluate(t3_estim_loc_tracemap);
             
-%              disp ('Training Hybrid...')
-%             my_hybridTrainer = HybridTrainer;
-%             my_hybridTrainer.estimator = obj.hybridEstimator;
-%             %TODO: reshape?
-%             locErrors = vecnorm(trueLoc_train - estim_loc_train);
-%             hybridFKM = my_hybridTrainer.train(...
-%                 locFreeFeatures_train, estim_loc_train, locErrors, channelForPairsTr);
-%             disp('Evaluating Hybrid at validation set...')
-%             hybridMapEstimate = hybridFKM.evaluate(...
-%                 locFreeFeat_tracemap, estim_loc_tracemap);
-%             disp('Done');
+            %%
+            if obj.b_trainLocBased || obj.b_trainHybrid
+                if obj.b_syntheticLocError
+                    warning('just simulating location algorithm by adding random noise')
+                    
+                    t3_estim_loc_train    = t3_trueLoc_train ...
+                        + obj.std_syntheticLocationNoise*randn(size(t3_trueLoc_train));
+                    t3_estim_loc_tracemap = t3_trueLoc_tracemap ...
+                        + obj.std_syntheticLocationNoise*randn(size(t3_trueLoc_train));
+                else
+                    disp 'estimating locations at train set...'
+                    m_estim_loc_tx = obj.locEstimator.estimateLocations(...
+                        locBasedFeatures_train(:,:,1));
+                    m_estim_loc_rx = obj.locEstimator.estimateLocations(...
+                        locBasedFeatures_train(:,:,2));
+                    % t3_estimatedLocation is a 3-way tensor:
+                    % (space_dim_index, sample_index, rx_or_tx)
+                    t3_estim_loc_train = cat(3, m_estim_loc_tx, m_estim_loc_rx);
+                    
+                    disp('estimating locations at tracemap (test) set...')
+                    estim_loc_tm_tx = obj.locEstimator.estimateLocations(...
+                        locBasedFeat_tracemap(:,:,1)); %estimated loc (tracemap)
+                    estim_loc_tm_rx = obj.locEstimator.estimateLocations(...
+                        locBasedFeat_tracemap(:,:,2));
+                    % t3_estim_loc_tracemap is a 3-way tensor:
+                    % (space_dim_index, sample_index, rx_or_tx)
+                    t3_estim_loc_tracemap = cat(3, estim_loc_tm_tx, estim_loc_tm_rx);
+                end
 
-            %% compute error measures
-            SqErr_locFree = Simulator.compute_SqErr(...
-                channelForPairs_tracemap, locFreeMapEstimate);
-            NMSE_locFree = SqErr_locFree/sum((channelForPairs_tracemap...
-                -mean(channelForPairs_tracemap)).^2);
-            SqErr_locBased = Simulator.compute_SqErr(...
-                channelForPairs_tracemap, locBasedMapEstimate);
-            NMSE_locBased = SqErr_locBased/sum((channelForPairs_tracemap...
-                -mean(channelForPairs_tracemap)).^2);
-%             SqErr_hybrid = Simulator.compute_SqErr(...
-%                 channelForPairs_tracemap, hybridMapEstimate);
-%             NMSE_hybrid = SqErr_hybrid/sum((channelForPairs_tracemap...
-%                 -mean(channelForPairs_tracemap)).^2);
-            hybridMapEstimate = 0;
-            NMSE_hybrid =0;
+            end
+            
+            %%
+            if obj.b_trainLocBased
+                disp ('Training LocBased...')
+                my_locBasedTrainer = LocBasedTrainer;
+                my_locBasedTrainer.estimator = obj.locBasedEstimator;
+                %%
+                v_lambdas_toTryLB = logspace(-5, -2,  15);   %TODO: move to properties
+                v_sigmas_toTryLB  = linspace(20, 100, 12);   %TODO: move to properties
+                if obj.b_cv
+                    disp ('Cross-validation...')
+                    [m_crossValScoresLB, best_lambdaLB, best_sigmaLB] = ...
+                        my_locBasedTrainer.crossValidate(...
+                        t3_estim_loc_train, channelForPairsTr, ...
+                        v_lambdas_toTryLB, v_sigmas_toTryLB);
+                    my_locBasedTrainer.estimator.regularizationParameter = best_lambdaLB;
+                    my_locBasedTrainer.estimator.kernel =  @(x, y) ...
+                        exp(-norms(x-y, 2, 1).^2/(best_sigmaLB^2));
+                    figure(999); clf
+                    mesh(v_sigmas_toTryLB, v_lambdas_toTryLB, m_crossValScoresLB); 
+                    ax = gca;
+                    ax.YAxis.Scale = 'log';
+                    xlabel \sigma
+                    ylabel \lambda
+                    title 'LOCATION BASED'
+                end % save por_siacaso
+                
+                %%
+                locBasedFKM = my_locBasedTrainer.train(...
+                    t3_estim_loc_train,channelForPairsTr);
+                
+                str_mapEstimates.locBased = locBasedFKM.evaluate(t3_estim_loc_tracemap);
+                str_NMSE.locBased = obj.compute_NMSE(...
+                    channelForPairs_tracemap, str_mapEstimates.locBased);
+            else
+                str_NMSE.locBased = 1;
+                str_mapEstimates.locBased = zeros(size(channelForPairs_tracemap));
+            end
+            
+            %%
+            if obj.b_trainHybrid
+                
+                my_hybridTrainer = HybridTrainer;
+                my_hybridTrainer.hybridEstimator = obj.hybridEstimator;
+                if obj.b_cv
+                    disp(['Hybrid model inherits lambda and sigma from '...
+                        'the pure loc-Free and the pure locBased']);
+                    my_hybridTrainer.hybridEstimator.h_kernelLB = @(x, y) ...
+                        exp(-norms(x-y, 2, 1).^2/(best_sigmaLB^2));
+                    my_hybridTrainer.hybridEstimator.regularizationParameterLB = best_lambdaLB;
+                    my_hybridTrainer.hybridEstimator.h_kernelLF = @(x, y) ...
+                        exp(-norms(x-y, 2, 1).^2/(best_sigmaLF^2));
+                    my_hybridTrainer.hybridEstimator.regularizationParameterLF = best_lambdaLF;
+                end
+                
+                disp ('Training Hybrid...')
+                snr_locErrors = 20;
+                locErrors = squeeze(vecnorm(t3_trueLoc_train - t3_estim_loc_train));
+                locErrors = locErrors + randn(size(locErrors))*...
+                    std(locErrors(:))/snr_locErrors;
+                % TODO: using the true location to compute the locErrors is "cheating"
+                % we must do it using only the positioning pilots
+                hybridFKM = my_hybridTrainer.train(...
+                locFreeFeatures_train, t3_estim_loc_train, locErrors, channelForPairsTr);
+                
+                disp('Evaluating Hybrid at validation set...')
+                locErrors_val = squeeze(vecnorm(t3_trueLoc_tracemap - t3_estim_loc_tracemap));
+                locErrors_val = locErrors_val + randn(size(locErrors_val))*...
+                    std(locErrors(:))/snr_locErrors;
+                str_mapEstimates.hybrid = hybridFKM.evaluate(...
+                    locFreeFeat_tracemap, t3_estim_loc_tracemap, locErrors_val);
+                %
+                str_mapEstimates.locFree_fromHybrid = hybridFKM.fkm_lf.evaluate(...
+                    locFreeFeat_tracemap);
+                str_mapEstimates.locBased_fromHybrid = hybridFKM.fkm_lb.evaluate(...
+                    t3_estim_loc_tracemap);
+                %
+                disp('Done');
+                str_NMSE.hybrid =             obj.compute_NMSE(...
+                    channelForPairs_tracemap, str_mapEstimates.hybrid);
+                str_NMSE.locFree_fromHybrid = obj.compute_NMSE(...
+                    channelForPairs_tracemap, str_mapEstimates.locFree_fromHybrid);
+                str_NMSE.locBased_fromHybrid = obj.compute_NMSE(...
+                    channelForPairs_tracemap, str_mapEstimates.locBased_fromHybrid);
+            else
+                str_NMSE.hybrid              = 1;
+                str_NMSE.locFree_fromHybrid  = 1;
+                str_NMSE.locBased_fromHybrid = 1;
+                [str_mapEstimates.hybrid, str_mapEstimates.locBased_fromHybrid,  ...
+                    str_mapEstimates.locFree_fromHybrid  ] ...
+                    = deal(zeros(size(channelForPairs_tracemap)));                
+            end
+        end
+    end
+    methods(Static)
+        function NMSE = compute_NMSE(trueMap, estimatedMap)
+            sqErr = Simulator.compute_SqErr(...
+                    trueMap(:), estimatedMap(:));
+                NMSE = sqErr/sum((trueMap - mean(trueMap)).^2);
         end
     end
 end
