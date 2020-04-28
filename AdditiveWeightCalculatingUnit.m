c948bb72b1759dc55a44719ba454a995e6d9962d
Luis Miguel Lopez <luismiguel.lopez@uia.no>
2020-04-28 15:14:40 +0200

    created class WeightCalculatingUnit and additive subclass

---
+++ AdditiveWeightCalculatingUnit.m
@@ -0,0 +1,17 @@
+classdef AdditiveWeightCalculatingUnit < WeightCalculatingUnit
+    properties
+        v_locErrors
+        v_weights
+    end
+    
+    methods
+        function v_w_out = evalWeights(obj, m_locErrors)
+            assert(size(m_locErrors,2 )==2);
+            v_locErrors_t = m_locErrors(:,1);
+            v_locErrors_r = m_locErrors(:,2);
+            v_wt = interp1(obj.v_locErrors, obj.v_weights, v_locErrors_t);
+            v_wr = interp1(obj.v_locErrors, obj.v_weights, v_locErrors_r);
+            v_w_out = (v_wt + v_wr)/2;
+        end
+    end
+end
---
+++ WeightCalculatingUnit.m
@@ -0,0 +1,8 @@
+classdef WeightCalculatingUnit
+   
+    
+    methods
+        v_w_out = evalWeights(m_locErrors)
+    end
+end
+
