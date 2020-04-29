b7575206566e59243665c84fccd2cd87f652466a
Luis Miguel Lopez <luismiguel.lopez@uia.no>
2020-04-29 15:23:27 +0200

    AdditiveWCU now extrapolates

--- ProcessingBlocks/Estimators/Tools/AdditiveWeightCalculatingUnit.m
+++ ProcessingBlocks/Estimators/Tools/AdditiveWeightCalculatingUnit.m
@@ -9,9 +9,12 @@ classdef AdditiveWeightCalculatingUnit < WeightCalculatingUnit
             assert(size(m_locErrors,2 )==2);
             v_locErrors_t = m_locErrors(:,1);
             v_locErrors_r = m_locErrors(:,2);
-            v_wt = interp1(obj.v_locErrors, obj.v_weights, v_locErrors_t);
-            v_wr = interp1(obj.v_locErrors, obj.v_weights, v_locErrors_r);
+            v_wt = interp1(obj.v_locErrors, obj.v_weights, ...
+                v_locErrors_t, 'linear', 'extrap');
+            v_wr = interp1(obj.v_locErrors, obj.v_weights, ...
+                v_locErrors_r, 'linear', 'extrap');
             v_w_out = (v_wt + v_wr)/2;
         end
+                        
     end
 end
