970cfab8f68d12eda6bcbb052933dbb2607f680a
Luis Miguel Lopez <luismiguel.lopez@uia.no>
2020-05-28 12:32:23 +0200

    whichExp takes funbasename, and produces warnings if fails

--- Execution/utilities/whichExp.m
+++ Execution/utilities/whichExp.m
@@ -1,6 +1,16 @@
 function ch_expNumber = whichExp()
-str_stack = dbstack;
-theClass = str_stack(2).file(1:end-1);
-ch_expNumber = char(extractAfter(...
-    str_stack(2).name, theClass + "experiment_"));
-
+ch_expNumber = '';
+try
+    obj = evalin('caller', 'obj');
+    str_stack = dbstack;
+    theClass = str_stack(2).file(1:end-1);
+    ch_expNumber = char(extractAfter(...
+    str_stack(2).name, strcat(theClass, obj.funbasename)));
+catch me
+    switch me.identifier
+        case 'MATLAB:UndefinedFunction'
+            warning 'whichExp must be called within a method of a class inheriting from ExperimentFunctionSet'
+        case 'MATLAB:badsubscript'
+            warning 'whichExp must be called within a method of a class inheriting from ExperimentFunctionSet'
+    end
+end
